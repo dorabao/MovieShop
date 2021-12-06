eff63240baa68543d31ba81dc8163cb5179265c4
dorabao <dorriebao@gmail.com>
2021-12-05 21:53:30 -0800

    Add project files.

---
+++ ApplicationCore/RepositoryInterfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.RepositoryInterfaces
+{
+    // T is template with generic contraint where T should be a entity class
+    public interface IRepository<T> where T : class
+    {
+        T GetById(int id);
+        List<T> GetAll();
+        T Add(T entity);
+        T Update(T entity);
+        T Delete(int id);
+
+    }
+}
---
+++ Infrastructure/Services/MovieMockService.cs
@@ -0,0 +1,35 @@
+﻿using ApplicationCore.Models;
+using ApplicationCore.RepositoryInterfaces;
+using ApplicationCore.ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class MovieMockService : IMovieService
+    {
+        private IMovieRepository _movieRepository;
+        public MovieMockService(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+        public IEnumerable<MovieCardResponseModel> GetHighestGrossingMovies()
+        {
+            // call my MovieRepository and get the data
+            var movies = _movieRepository.Get30HighestGrossingMovies();
+            // 3rd party Automapper from Nuget
+            var movieCards = new List<MovieCardResponseModel>();
+            foreach (var movie in movies)
+            {
+                movieCards.Add(
+                    new MovieCardResponseModel { Id = movie.Id, PosterUrl = movie.PosterUrl, Title = movie.Title }
+                    );
+            }
+
+            return movieCards;
+        }
+    }
+}
