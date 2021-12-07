0767ca5bd772d770d99f98105082e89c5f7f7362
dorabao <dorriebao@gmail.com>
2021-12-07 00:52:15 -0800

    Createing User/Role/Prchase/Favorite/Cast/Crew tables and updated databse

---
+++ Infrastructure/Migrations/20211207085008_CreatingUser_Role_Purchase_Favorite_Cast_CrewTables.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class CreatingUser_Role_Purchase_Favorite_Cast_CrewTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
